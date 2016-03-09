7b62a798086613a53c22e19053af12954a4f0688
ymyang <ymyang@163.com>
2016-03-09 22:22:50 +0800

    first commit

---
+++ dist/ng-safeapply.js
@@ -0,0 +1,21 @@
+/**
+ * Created by yang on 2016/3/9.
+ */
+angular.module('ng-safeapply', []).factory('SafeApply', function() {
+
+    return function(scope, fn) {
+        var phase = scope.$root.$$phase;
+        if (phase == '$apply' || phase == '$digest') {
+            if (fn) {
+                scope.$eval(fn);
+            }
+        } else {
+            if (fn) {
+                scope.$apply(fn);
+            } else {
+                scope.$apply();
+            }
+        }
+    };
+
+});
